612604603919d84aa0280d6889abe4af73d1a699
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-05-10 10:03:56 +0530

    Add untracked files

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,11 @@
+import testimonials from './testimonials'
+import contact from './contact'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import Experience from './experiences'
+
+
+export const schemaTypes = [testimonials,Experience,contact,skills,workExperience,brands,works,abouts]
